339d2485ab50f094a1fc5e8da2307998c51a7bfb
Diana Dashynevich <ddashinevich@gmail.com>
2025-02-24 11:19:49 +0100

    Initial Commit

---
+++ Wallpapers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wallpapers
+//
+//  Created by Diana Dashinevich on 24/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wallpapers/WallpapersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpapersApp.swift
+//  Wallpapers
+//
+//  Created by Diana Dashinevich on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WallpapersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
