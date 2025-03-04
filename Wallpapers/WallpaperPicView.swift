c454d98c5a55ec21d60844fa1d0b4f88de20df08
Diana Dashynevich <ddashinevich@gmail.com>
2025-03-04 21:01:22 +0100

    feat: implemented functionality to save wallpapers to the photo library, added permission request for saving images

--- Wallpapers/WallpaperPicView.swift
+++ Wallpapers/WallpaperPicView.swift
@@ -5,48 +5,69 @@
 //  Created by Diana Dashinevich on 25/02/2025.
 //
 import SwiftUI
+import Kingfisher
+import Photos
 
 struct WallpaperPicView: View {
     let wallpaper: Wallpaper
+    let photoSaver = PhotoSaver()
     
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack {
-                    //                Text(wallpaper.title)
-                    //                    .font(.headline)
-                    //                    .foregroundColor(.primary)
-                    //
-                    HStack {
-                        ForEach(wallpaper.tags, id: \.self) { tag in
-                            Text(tag)
-                                .padding(.horizontal, 10)
-                                .padding(.vertical, 5)
-                                .background(Color.gray.opacity(0.2))
-                                .clipShape(Capsule())
-                                .font(.caption)
+                    ScrollView (.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(wallpaper.tags, id: \.self) { tag in
+                                Text(tag)
+                                    .padding(.horizontal, 10)
+                                    .padding(.vertical, 5)
+                                    .background(Color.gray.opacity(0.2))
+                                    .clipShape(Capsule())
+                                    .font(.caption)
+                            }
                         }
-                        Spacer()
                     }
                     .padding(.horizontal)
-                    AsyncImage(url: URL(string: wallpaper.imageUrl)) { image in
-                        image
+                    
+                    if let url = URL(string: wallpaper.imageUrl) {
+                        KFImage(url)
                             .resizable()
                             .scaledToFill()
                             .clipShape(RoundedRectangle(cornerRadius: 20))
-                            .padding()
                             
-                    } placeholder: {
+                        
+                        Button(action: {
+                            guard let url = URL(string: wallpaper.imageUrl) else { return }
+                            
+                            KingfisherManager.shared.retrieveImage(with: url) { result in
+                                switch result {
+                                case .success(let value):
+                                    let image = value.image
+                                    let photoSaver = PhotoSaver() 
+                                    photoSaver.saveImageToGallery(image: image)
+                                case .failure(let error):
+                                    print("Error downloading image: \(error.localizedDescription)")
+                                }
+                            }
+                        }) {
+                            Text("Save to Photos")
+                                .padding()
+                                .background(Color.black)
+                                .foregroundColor(.white)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                        }
+                        .padding()
+                    } else {
                         ProgressView()
                     }
-                    
                 }
+                .padding(.bottom, 70)
             }
             .navigationBarTitle(wallpaper.title)
         }
     }
 }
-
 
 
 
