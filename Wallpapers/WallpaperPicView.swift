b69cc10c8f3a0edbbfc71af312364af755dee3ca
Diana Dashynevich <ddashinevich@gmail.com>
2025-03-05 12:21:36 +0100

    feat: improve UI

--- Wallpapers/WallpaperPicView.swift
+++ Wallpapers/WallpaperPicView.swift
@@ -11,61 +11,72 @@ import Photos
 struct WallpaperPicView: View {
     let wallpaper: Wallpaper
     let photoSaver = PhotoSaver()
+    @EnvironmentObject var favorites: Favorites
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack {
-                    ScrollView (.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(wallpaper.tags, id: \.self) { tag in
-                                Text(tag)
-                                    .padding(.horizontal, 10)
-                                    .padding(.vertical, 5)
-                                    .background(Color.gray.opacity(0.2))
-                                    .clipShape(Capsule())
-                                    .font(.caption)
-                            }
+        ScrollView {
+            VStack {
+                ScrollView (.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(wallpaper.tags, id: \.self) { tag in
+                            Text(tag)
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 5)
+                                .background(Color.gray.opacity(0.2))
+                                .clipShape(Capsule())
+                                .font(.caption)
                         }
                     }
-                    .padding(.horizontal)
-                    
-                    if let url = URL(string: wallpaper.imageUrl) {
-                        KFImage(url)
-                            .resizable()
-                            .scaledToFill()
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                            
+                }
+                .padding(.horizontal)
+                
+                if let url = URL(string: wallpaper.imageUrl) {
+                    KFImage(url)
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .padding(10)
+                } else {
+                    ProgressView()
+                }
+                HStack {
+                    Button(action: {
+                        guard let url = URL(string: wallpaper.imageUrl) else { return }
                         
-                        Button(action: {
-                            guard let url = URL(string: wallpaper.imageUrl) else { return }
-                            
-                            KingfisherManager.shared.retrieveImage(with: url) { result in
-                                switch result {
-                                case .success(let value):
-                                    let image = value.image
-                                    let photoSaver = PhotoSaver() 
-                                    photoSaver.saveImageToGallery(image: image)
-                                case .failure(let error):
-                                    print("Error downloading image: \(error.localizedDescription)")
-                                }
+                        KingfisherManager.shared.retrieveImage(with: url) { result in
+                            switch result {
+                            case .success(let value):
+                                let image = value.image
+                                let photoSaver = PhotoSaver()
+                                photoSaver.saveImageToGallery(image: image)
+                            case .failure(let error):
+                                print("Error downloading image: \(error.localizedDescription)")
                             }
-                        }) {
-                            Text("Save to Photos")
-                                .padding()
-                                .background(Color.black)
-                                .foregroundColor(.white)
-                                .clipShape(RoundedRectangle(cornerRadius: 20))
                         }
-                        .padding()
-                    } else {
-                        ProgressView()
+                    }) {
+                        Text("Save to Photos")
+                            .padding()
+                            .background(Color.black)
+                            .foregroundColor(.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
                     }
+                    Button(favorites.contains(wallpaper) ? "Remove from Favorites" : "Add to Favorites") {
+                                        if favorites.contains(wallpaper) {
+                                            favorites.remove(wallpaper)
+                                        } else {
+                                            favorites.add(wallpaper)
+                                        }
+                                    }
+                                    .padding()
+                                    .background(Color.black)
+                                    .foregroundColor(.white)
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
                 }
                 .padding(.bottom, 70)
             }
-            .navigationBarTitle(wallpaper.title)
         }
+        .navigationTitle(wallpaper.title)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
@@ -81,4 +92,5 @@ struct WallpaperPicView: View {
             thumbnailUrl: "https://example.com/thumb.jpg"
         )
     )
+    .environmentObject(Favorites())
 }
